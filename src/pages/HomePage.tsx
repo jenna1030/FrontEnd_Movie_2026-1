2408f6567b61803f602d81c5d329f7c38c368486
jenna <jihyeonie@gmail.com>
2026-07-09 03:47:58 +0900

    refactor: 영화 목록 조회에 useQuery 적용

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
 import { fetchPopularMovies } from "../apis/popularMovieApi";
 import { searchMovies } from "../apis/searchMovieApi";
 import { fetchMovieDetail } from "../apis/movieDetailApi";
@@ -6,6 +8,7 @@ import Header from "../components/Header/Header";
 import MovieList from "../components/MovieList/MovieList";
 import MovieModal from "../components/MovieModal/MovieModal";
 import type { Movie, MovieDetail } from "../types/movie";
+
 import "../App.css";
 
 function HomePage() {
@@ -15,14 +18,15 @@ function HomePage() {
   const [searchKeyword, setSearchKeyword] = useState("");
   const [selectedMovie, setSelectedMovie] = useState<MovieDetail | null>(null);
 
-  useEffect(() => {
-    async function loadPopularMovies() {
-      const popularMovies = await fetchPopularMovies(1);
-      setMovies(popularMovies);
-    }
-
-    loadPopularMovies();
-  }, []);
+  const {
+    data: popularMovies = [],
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["movies", "popular", 1],
+    queryFn: () => fetchPopularMovies(1),
+  });
 
   async function handleLoadMore() {
     const nextPage = page + 1;
@@ -31,7 +35,11 @@ function HomePage() {
       ? await searchMovies(searchKeyword, nextPage)
       : await fetchPopularMovies(nextPage);
 
-    setMovies((prevMovies) => [...prevMovies, ...nextMovies]);
+    setMovies((prevMovies) => [
+      ...(prevMovies.length > 0 ? prevMovies : popularMovies),
+      ...nextMovies,
+    ]);
+
     setPage(nextPage);
   }
 
@@ -60,6 +68,16 @@ function HomePage() {
     ? `"${searchKeyword}" 검색 결과`
     : "지금 인기있는 영화";
 
+  const displayedMovies = movies.length > 0 ? movies : popularMovies;
+
+  if (isPending) {
+    return <p>영화 목록을 불러오는 중입니다...</p>;
+  }
+
+  if (isError) {
+    return <p>에러가 발생했습니다: {error.message}</p>;
+  }
+
   return (
     <>
       <Header
@@ -70,7 +88,7 @@ function HomePage() {
 
       <main className="main-content">
         <MovieList
-          movies={movies}
+          movies={displayedMovies}
           title={movieListTitle}
           onLoadMore={handleLoadMore}
           onMovieClick={handleMovieClick}
